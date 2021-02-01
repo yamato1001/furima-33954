c5ef66c33918ea770f25e7b62cf4a26ee71726de
yamato1001 <yamato1001apa@i.softbank.jp>
2021-02-01 14:48:16 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
+    validates :first_name_kana, :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name, :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :birthday
+  end
 end
--- db/migrate/20210129091119_devise_create_users.rb
+++ db/migrate/20210129091119_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.date   :birthday,           null: false
       
 
 
