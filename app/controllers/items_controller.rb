737058b8efa0bf4a148bd4c14f578b03a0f06a5b
yamato1001 <yamato1001apa@i.softbank.jp>
2021-02-08 16:16:26 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,20 +23,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
 
@@ -44,11 +48,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :concept, :category_id, :product_condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id:current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     unless @item.user.id == current_user.id
     redirect_to root_path
     end
   end
+
+  
 
 end
